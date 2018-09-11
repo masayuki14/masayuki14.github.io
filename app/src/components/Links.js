193b7e46e3a93ad5198610664ba7b08abd5266e6
Masayuki Morisaki <masayuki144@gmail.com>
2018-09-11 17:30:32 +0900

    外部リンク用のコンポーネントを作成

---
+++ app/src/components/Links.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+let Links = function () {
+  return (
+    <div>
+      <p><a target="_blank" href="https://masayuki14.hatenablog.com/">blog</a></p>
+      <p><a target="_blank" href="https://twitter.com/masayuki14">twitter</a></p>
+      <p><a target="_blank" href="https://github.com/masayuki14/">github</a></p>
+      <p><a target="_blank" href="https://www.wantedly.com/users/74688">wantedly</a></p>
+      <p><a target="_blank" href="https://speakerdeck.com/masayuki14">speaker deck</a></p>
+      <p><a target="_blank" href="https://connpass.com/user/masayuki14/">connpass</a></p>
+    </div>
+  );
+}
+
+export default Links
